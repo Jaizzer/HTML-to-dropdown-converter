994bccb3086e5cebd071a82a0ba47c548f22353c
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-25 15:54:44 +0800

    feat: Add function that toggles the class of drop-down-items-container from hidden to not hidden

---
+++ main.js
@@ -0,0 +1,13 @@
+function turnToDropDown(dropDownButton) {
+
+    // Get drop down items container inside the drop down button.
+    const dropDownItemsContainer = dropDownButton.querySelector('.drop-down-items-container');
+    dropDownItemsContainer.classList.toggle('hidden');
+
+    // Hide or unhide drop-down button's  drop-down items.
+    dropDownButton.addEventListener('click', () => {
+        dropDownItemsContainer.classList.toggle('hidden');
+    });
+}
+
+turnToDropDown(document.querySelector('.drop-down'));
