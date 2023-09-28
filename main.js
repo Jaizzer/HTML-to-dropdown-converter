cc3c94de03a9f90b5f635b2dd9dba873523646a3
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-28 08:55:42 +0800

    feat: Eliminate reliance on css to hide drop-down

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ function turnToDropDown(dropDownButton) {
 
     // Get drop down items container inside the drop down button.
     const dropDownItemsContainer = dropDownButton.querySelector('.drop-down-items-container');
-    dropDownItemsContainer.classList.toggle('hidden');
+    dropDownItemsContainer.style.display = 'none';
 
     // Hide or unhide drop-down button's  drop-down items.
     dropDownButton.addEventListener('click', () => {
-        dropDownItemsContainer.classList.toggle('hidden');
+        if (dropDownItemsContainer.style.display !== 'none') {
+            dropDownItemsContainer.style.display = 'none';
+        } else {
+            dropDownItemsContainer.style.display = 'initial';
+        }
     });
 }
